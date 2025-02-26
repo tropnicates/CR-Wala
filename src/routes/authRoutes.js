007f6ecaf408bf42041f63172c4828792284347e
Aarya34 <142531338+Aarya34@users.noreply.github.com>
2025-02-26 23:08:47 +0530

    Added google authentication

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import passport from "passport";
 import { register, login, logout, getProfile, editProfile,  forgotPassword, resetPassword,getAllUsers, changeUserStatusActive,changeUserStatusInactive } from '../controllers/authController.js';
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
 // import passport from "passport";
@@ -26,20 +27,29 @@ router.get('/',getAllUsers);
 router.put('/:id/active',changeUserStatusActive);
 router.put('/:id/inactive',changeUserStatusInactive);
 
-
 // router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
 
-// router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
 
-// router.get(
-//   "/google/callback",
-//   passport.authenticate("google", { failureRedirect: "/login" }),
-//   (req, res) => {
-//     const token = generateToken(req.user);
-//     res.json({ success: true, message: "Google login successful", token, user: req.user });
-//   }
-// );
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  (req, res) => {
+    const token = generateToken(req.user);
+    res.json({ success: true, message: "Google login successful", token, user: req.user });
+  }
+);
 
+router.get("/apple", passport.authenticate("apple", { scope: ["profile", "email"] }));
+
+router.get(
+  "/apple/callback",
+  passport.authenticate("apple", { failureRedirect: "/login" }),
+  (req, res) => {
+    const token = generateToken(req.user);
+    res.json({ success: true, message: "Apple login successful", token, user: req.user });
+  }
+);
 
 
 export default router;
