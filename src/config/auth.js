e5aa2eabad62e9892020ac1fa64b188ca01f3349
Aarya34 <142531338+Aarya34@users.noreply.github.com>
2025-02-27 17:10:06 +0530

    Added Enquiry Api

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,42 +1,42 @@
-import passport from "passport";
-import { Strategy as GoogleStrategy } from "passport-google-oauth20";
-import dotenv from 'dotenv';
-dotenv.config();
+// import passport from "passport";
+// import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+// import dotenv from 'dotenv';
+// dotenv.config();
 
-// import AppleStrategy from "passport-apple";
+// // import AppleStrategy from "passport-apple";
 
-console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
-console.log("GOOGLE_CLIENT_SECRET:", process.env.GOOGLE_CLIENT_SECRET);
+// console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
+// console.log("GOOGLE_CLIENT_SECRET:", process.env.GOOGLE_CLIENT_SECRET);
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback",
-    scope: ["profile", "email"],
-  },
-  function(accessToken, refreshToken, profile, done) {
-    // Handle user profile here
-    return done(null, profile);
-  }
-));
-
-// passport.use(new AppleStrategy({
-//     clientID: process.env.APPLE_CLIENT_ID,
-//     teamID: process.env.APPLE_TEAM_ID,
-//     keyID: process.env.APPLE_KEY_ID,
-//     privateKey: process.env.APPLE_PRIVATE_KEY,
-//     callbackURL: "/auth/apple/callback"
+// passport.use(new GoogleStrategy({
+//     clientID: process.env.GOOGLE_CLIENT_ID,
+//     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     callbackURL: "/auth/google/callback",
+//     scope: ["profile", "email"],
 //   },
-//   function(accessToken, refreshToken, idToken, profile, done) {
+//   function(accessToken, refreshToken, profile, done) {
 //     // Handle user profile here
 //     return done(null, profile);
 //   }
 // ));
 
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
+// // passport.use(new AppleStrategy({
+// //     clientID: process.env.APPLE_CLIENT_ID,
+// //     teamID: process.env.APPLE_TEAM_ID,
+// //     keyID: process.env.APPLE_KEY_ID,
+// //     privateKey: process.env.APPLE_PRIVATE_KEY,
+// //     callbackURL: "/auth/apple/callback"
+// //   },
+// //   function(accessToken, refreshToken, idToken, profile, done) {
+// //     // Handle user profile here
+// //     return done(null, profile);
+// //   }
+// // ));
 
-passport.deserializeUser((obj, done) => {
-  done(null, obj);
-});
+// passport.serializeUser((user, done) => {
+//   done(null, user);
+// });
+
+// passport.deserializeUser((obj, done) => {
+//   done(null, obj);
+// });
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import passport from "passport";
-import { register, login, logout, getProfile, editProfile,  forgotPassword, resetPassword,getAllUsers, changeUserStatusActive,changeUserStatusInactive } from '../controllers/authController.js';
+import { register, login, logout, getProfile, editProfile,  forgotPassword, resetPassword,getAllUsers, changeUserStatusActive,changeUserStatusInactive,sendEnquiry} from '../controllers/authController.js';
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
 // import passport from "passport";
 import jwt from "jsonwebtoken";
@@ -26,30 +26,31 @@ router.post('/reset-password/:token', resetPassword);
 router.get('/',getAllUsers);
 router.put('/:id/active',changeUserStatusActive);
 router.put('/:id/inactive',changeUserStatusInactive);
+router.post('/enquiry', sendEnquiry);
 
 // router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
 
-router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+// router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
 
-router.get(
-  "/google/callback",
-  passport.authenticate("google", { failureRedirect: "/login" }),
-  (req, res) => {
-    const token = generateToken(req.user);
-    res.json({ success: true, message: "Google login successful", token, user: req.user });
-  }
-);
+// router.get(
+//   "/google/callback",
+//   passport.authenticate("google", { failureRedirect: "/login" }),
+//   (req, res) => {
+//     const token = generateToken(req.user);
+//     res.json({ success: true, message: "Google login successful", token, user: req.user });
+//   }
+// );
 
-router.get("/apple", passport.authenticate("apple", { scope: ["profile", "email"] }));
+// router.get("/apple", passport.authenticate("apple", { scope: ["profile", "email"] }));
 
-router.get(
-  "/apple/callback",
-  passport.authenticate("apple", { failureRedirect: "/login" }),
-  (req, res) => {
-    const token = generateToken(req.user);
-    res.json({ success: true, message: "Apple login successful", token, user: req.user });
-  }
-);
+// router.get(
+//   "/apple/callback",
+//   passport.authenticate("apple", { failureRedirect: "/login" }),
+//   (req, res) => {
+//     const token = generateToken(req.user);
+//     res.json({ success: true, message: "Apple login successful", token, user: req.user });
+//   }
+// );
 
 
 export default router;
