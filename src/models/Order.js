c66b808f70dab97861faf5f1b851e375919d3ab9
Kundan Kumar <82524425+tropnicates@users.noreply.github.com>
2025-03-05 13:46:45 +0530

    Update Order.js

--- src/models/Order.js
+++ src/models/Order.js
@@ -3,16 +3,34 @@ import mongoose from "mongoose";
 const orderSchema = mongoose.Schema(
     {
         user: {
-            type: mongoose.Schema.Types.ObjectId,
+            type: String,
             required: true,
             ref: "User",
         },
-        subtotal: {
-            type: Number,
-            required: true,
-            default: 0.0,
+        orderItems: [
+            {
+                name: { type: String, required: true },
+                qty: { type: Number, required: true },
+                price: { type: Number, required: true },
+            },
+        ],
+        shippingAddress: {
+            address: { type: String, required: true },
+            city: { type: String, required: true },
+            postalCode: { type: String, required: true },
+            country: { type: String, required: true },
         },
-        total: {
+        paymentMethod: {
+            type: String,
+            required: true,
+        },
+        paymentResult: {
+            id: { type: String },
+            status: { type: String },
+            update_time: { type: String },
+            email_address: { type: String },
+        },
+        totalPrice: {
             type: Number,
             required: true,
             default: 0.0,
